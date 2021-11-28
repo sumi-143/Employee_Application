c3b2d683bfdf0e21fc87800919888774390cd27e
sumi-143 <41041271+sumi-143@users.noreply.github.com>
2021-11-28 19:46:16 +0530

    Update Constants.java

--- Email_Application_V1/src/com/greatlearning/utils/Constants.java
+++ Email_Application_V1/src/com/greatlearning/utils/Constants.java
@@ -2,8 +2,9 @@ package com.greatlearning.utils;
 
 public final class Constants {
 
-	public Constants() {
-		// TODO Auto-generated constructor stub
-	}
+	public static final String Admin_Department="admin";
+	public static final String Hr_Department="Hr";
+	public static final String Legal_Department="legal";
+	public static final String Tech_Department="tech";
 
 }
